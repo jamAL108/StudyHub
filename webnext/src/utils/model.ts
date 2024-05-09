ffd22db93374b6336ef572496e18e04600675700
Jamal Mydeen <jamalmydeen112@gmail.com>
2024-05-10 00:49:58 +0530

    new commit

--- webnext/src/utils/model.ts
+++ webnext/src/utils/model.ts
@@ -1,7 +1,9 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
+const apiKey = process.env.NEXT_PUBLIC_API_KEY || '';
+
 const ModelConfig = () => {
-    const genAI = new GoogleGenerativeAI("AIzaSyBX16wrIG9mPvTXSc9iDA35v70phX7qgqg");
+    const genAI = new GoogleGenerativeAI(apiKey);
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
     return model;
 }
