3e8c857b5f61e41e6c180f36325326c181920831
Jamal Mydeen <jamalmydeen112@gmail.com>
2024-05-15 22:19:13 +0530

    Update route.ts

--- webnext/src/app/auth/callback/route.ts
+++ webnext/src/app/auth/callback/route.ts
@@ -12,7 +12,7 @@ export async function GET(request: Request) {
   const next = searchParams.get('next') ?? '/'
 
   // const origin ='https://thevotum.com'
-  const origin = 'http://localhost:3000'
+  const origin = 'https://vidchat-ai.vercel.app'
   console.log(origin)
 
   if (code) {
